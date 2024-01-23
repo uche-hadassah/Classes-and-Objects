9a9cc6673da17ab383c6d9773b164ced64104a1c
Hadassah Uche <uhadassah1@gmail.com>
2024-01-23 10:59:23 +0200

    added all the function implementations, now im completely done with the a part

--- UcheHadassah10_3/UcheHadassah10_3/HadassahUche10_3.cpp
+++ UcheHadassah10_3/UcheHadassah10_3/HadassahUche10_3.cpp
@@ -32,3 +32,54 @@ Student::Student(string ID, double Grades[], int totalNumber)
 		grades[i] = Grades[i];
 	}
 }
+//Function to get student ID
+void Student::enterID()
+{
+	cout << "Enter student ID:";
+	cin >> studentID;
+}
+
+//Function to enter a single test grade and update the total number of grades entered
+void Student::enterTestGrade()
+{
+	if (total < 5)
+	{
+		cout << "Enter Test Grade " << total + 1 << ": ";
+		cin >> grades[total];
+		total++;
+	}
+	else
+	{
+		cout << "Cannot enter more than 5 grades." << endl;
+	}
+}
+
+//Function to display all the data of the student
+void Student::displayStudentData()
+{
+	cout << "Student ID: " << studentID << endl;
+	cout << "Test Grades: ";
+	for (int i = 0; i < total; ++i) 
+	{
+		cout << grades[i] << " ";
+	}
+	cout << endl;
+}
+
+//Function to compute and display the average grade along with the student ID
+void Student::averageGrade()
+{
+	if (total > 0)
+	{
+		double sum = 0;
+		for (int i = 0; i < total; ++i) {
+			sum += grades[i];
+		}
+		double average = sum / total;
+		cout << "Average Grade: " << average << endl;
+	}
+	else
+	{
+		cout << "No grades entered yet." << endl;
+	}
+}
