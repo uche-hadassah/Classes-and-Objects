846c9df9302712fa6d3f9c7d46a783160896558c
Hadassah Uche <uhadassah1@gmail.com>
2024-01-23 10:14:40 +0200

    This is what i have so far

--- UcheHadassah10_2/UcheHadassah10_2/HadassahUche10_2.cpp
+++ UcheHadassah10_2/UcheHadassah10_2/HadassahUche10_2.cpp
@@ -7,4 +7,4 @@ constructors: one with parameters and one without parameters).
 b) Include the class definition above in a program that declares two objects of type 
 Employee: emp1 and emp2 (using a different constructor for each one), and asks the 
 user to enter the required data for each of the two employees and then diplays the data 
-of each one.*/#include<iostream>using namespace std;class Employee{public:	Employee();	Employee(int employeeID, double employeePayRate, int maxHoursPerWeek);private:	int ID;	double payRate;	int hours;};
+of each one.*/#include<iostream>using namespace std;class Employee{public:	Employee();//Constructor without parameters	Employee(int employeeID, double employeePayRate, int maxHoursPerWeek);//Constructor with parameters	void enterData();//function to enter data for new employee	void displayData();//function to display data of an employeeprivate:	int ID;	double payRate;	int hours;};Employee::Employee():ID(0),payRate(0.0),hours(0){}Employee::Employee(int employeeID, double employeePayRate, int maxHoursPerWeek)	:ID(employeeID), payRate(employeePayRate), hours(maxHoursPerWeek){}void Employee::enterData(){}
