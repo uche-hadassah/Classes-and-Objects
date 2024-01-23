dba51c0cae3b2c2353ffef1a0fb640fddea5a5ca
Hadassah Uche <uhadassah1@gmail.com>
2024-01-23 10:29:44 +0200

    Its a wrap. The program runs smoothly

--- UcheHadassah10_2/UcheHadassah10_2/HadassahUche10_2.cpp
+++ UcheHadassah10_2/UcheHadassah10_2/HadassahUche10_2.cpp
@@ -1,10 +1,62 @@
-/* a) Construct a class named Employee that can be used to represent an employee of a 
-company. Each employee is defined by an integer ID number, a pay rate, and the 
-maximum number of hours the employee should work each week. The services 
-(functions) provided by the class should be: the ability to enter data for a new 
-employee, the ability to display data for an employee. (Your class should include two 
-constructors: one with parameters and one without parameters).
-b) Include the class definition above in a program that declares two objects of type 
-Employee: emp1 and emp2 (using a different constructor for each one), and asks the 
-user to enter the required data for each of the two employees and then diplays the data 
-of each one.*/#include<iostream>using namespace std;class Employee{public:	Employee();//Constructor without parameters	Employee(int employeeID, double employeePayRate, int maxHoursPerWeek);//Constructor with parameters	void enterData();//function to enter data for new employee	void displayData();//function to display data of an employeeprivate:	int ID;	double payRate;	int hours;};Employee::Employee():ID(0),payRate(0.0),hours(0){}Employee::Employee(int employeeID, double employeePayRate, int maxHoursPerWeek)	:ID(employeeID), payRate(employeePayRate), hours(maxHoursPerWeek){}void Employee::enterData(){}
+/* Name:Uche Hadassah
+* This project reads in the data for 2 employees and displays it
+*/
+#include<iostream>
+using namespace std;
+class Employee
+{
+public:
+	Employee();//Constructor without parameters
+	Employee(int employeeID, double employeePayRate, int maxHoursPerWeek);//Constructor with parameters
+
+	void enterData();//function to enter data for new employee
+	void displayData();//function to display data of an employee
+private:
+	int ID;
+	double payRate;
+	int hours;
+};
+
+//implementation of the Employee class
+Employee::Employee():ID(0),payRate(0.0),hours(0){}
+
+Employee::Employee(int employeeID, double employeePayRate, int maxHoursPerWeek)
+	:ID(employeeID), payRate(employeePayRate), hours(maxHoursPerWeek){}
+
+//Function to get employee data
+void Employee::enterData()
+{
+	cout << "Enter Employee ID: ";
+	cin >> ID;
+
+	cout << "Enter Pay Rate: ";
+	cin >> payRate;
+
+	cout << "Enter Maximum Hours per Week: ";
+	cin >> hours;
+}
+
+//Function to show employee data
+void Employee::displayData()
+{
+	cout << "Employee ID: " << ID << endl;
+	cout << "Pay Rate: $" << payRate << " per hour" << endl;
+	cout << "Maximum Hours per Week: " << hours << " hours" << endl;
+}
+int main()
+{
+	// Declare two objects of type Employee
+	Employee emp1;//The constructor without parameters will be called
+	Employee emp2(2,35.0,20);//The constructor with parameters will be called
+	
+	cout << "Enter the data for employee 1" << endl;
+	emp1.enterData();
+	cout << "Enter the data for employee 2" << endl;
+	emp2.enterData();
+	cout << "The data for the two employees are as follows:" << endl;
+	emp1.displayData();
+	cout << "--------------------" << endl;
+	emp2.displayData();
+	cout << "--------------------" << endl;
+	return 0;
+}
